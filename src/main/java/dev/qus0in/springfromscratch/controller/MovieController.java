271094270213b7e3dcbb9e51d3510d7b93f04a1a
qus0in <qus0in@gmail.com>
2025-03-17 15:43:12 +0900

    Gemini

--- src/main/java/dev/qus0in/springfromscratch/controller/MovieController.java
+++ src/main/java/dev/qus0in/springfromscratch/controller/MovieController.java
@@ -2,6 +2,7 @@ package dev.qus0in.springfromscratch.controller;
 
 import dev.qus0in.springfromscratch.model.dto.MovieDTO;
 import dev.qus0in.springfromscratch.model.dto.MovieInfoDTO;
+import dev.qus0in.springfromscratch.service.GeminiService;
 import dev.qus0in.springfromscratch.service.MovieService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,9 +15,11 @@ import java.util.List;
 @RequestMapping("/")
 public class MovieController {
     final MovieService movieService;
+    final GeminiService geminiService;
 
-    public MovieController(MovieService movieService) {
+    public MovieController(MovieService movieService, GeminiService geminiService) {
         this.movieService = movieService;
+        this.geminiService = geminiService;
     }
 
 
@@ -25,6 +28,9 @@ public class MovieController {
 //        List<MovieDTO> movies = movieService.getMovies();
         List<MovieInfoDTO> movies = movieService.getMovieInfos();
         model.addAttribute("movies", movies);
+        String prompt = "%s, 앞의 데이터를 바탕으로 영화를 추천하고 그 이유를 작성. no markdown, just plain-text, in korean language".formatted(movies.toString());
+        String recommendation = geminiService.callGemini(prompt);
+        model.addAttribute("recommendation", recommendation);
         return "index";
     }
 }
